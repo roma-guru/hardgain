9f2567302e80bacb41c1f892902d285e43de3268
Roman Voropaev <voropaev.roma@gmail.com>
2018-01-08 03:16:49 +0300

    Schedule improvements + much better admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from schedule.models import *
-admin.site.register(Exercise)
-admin.site.register(Cycle)
-admin.site.register(ScheduleDay)
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('html_img', 'is_base', 'name', 'desc', 'html_a')
+
+    def html_img(self, obj):
+        return format_html(f"<img height='100px' src='{obj.image_link}'></img>")
+    html_img.short_description = 'Image'
+
+    def html_a(self, obj):
+        return format_html(f"<a target='_blank' href='{obj.link}'>Link</a>")
+    html_a.short_description = 'Link'
+
+@admin.register(Cycle)
+class CycleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'start_date', 'end_date', 'completed')
+
+@admin.register(ScheduleDay)
+class ScheduleDay(admin.ModelAdmin):
+    list_display = ('cycle', 'weekday')
--- schedule/models.py
+++ schedule/models.py
@@ -7,9 +7,10 @@ class Exercise(models.Model):
     and is it base exercise (most important).
     """
     name:str = models.CharField(max_length=255)
-    desc:str = models.CharField(max_length=255)
-    image_link:str = models.CharField(max_length=1000, null=True)
+    desc:str = models.CharField(max_length=255, blank=True)
+    image_link:str = models.CharField(max_length=1000, blank=True)
     link:str = models.CharField(max_length=255, blank=True)
+    comment:str = models.TextField(blank=True)
     is_base:bool = models.BooleanField(default=False)
 
     def __str__(self):
@@ -23,7 +24,9 @@ class Cycle(models.Model):
     name:str = models.CharField(max_length=255, blank=True, default='')
     start_date:date = models.DateField()
     end_date:date = models.DateField(null=True)
-    comment:str = models.CharField(max_length=255, blank=True)
+    comment:str = models.TextField(blank=True)
+    active:bool = models.BooleanField(default=True)
+    completed:bool = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -34,18 +37,18 @@ class ScheduleDay(models.Model):
     scheduled exercises. Linked to Cycle.
     """
     WEEKDAYS = (
-        ('Mon', 'Monday'),
-        ('Tue', 'Tuesday'),
-        ('Wed', 'Wednesday'),
-        ('Thu', 'Thursday'),
-        ('Fri', 'Friday'),
-        ('Sat', 'Saturday'),
-        ('Sun', 'Sunday'),
+        ('Mon', 'Понедельник'),
+        ('Tue', 'Вторник'),
+        ('Wed', 'Среда'),
+        ('Thu', 'Четверг'),
+        ('Fri', 'Пятница'),
+        ('Sat', 'Суббота'),
+        ('Sun', 'Воскресение'),
     )
-    weekday = models.CharField(max_length=15, choices=WEEKDAYS)
-    comment = models.CharField(max_length=255, blank=True)
+    weekday = models.CharField(max_length=5, choices=WEEKDAYS)
     cycle = models.ForeignKey(Cycle, on_delete=models.CASCADE, related_name='schedule')
     exercises = models.ManyToManyField(Exercise)
+    comment:str = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.cycle}: {self.weekday}"

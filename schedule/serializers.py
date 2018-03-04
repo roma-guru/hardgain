fdc3c00cbe6ab2673a2aec6131ce89c7e74479cc
Roman Voropaev <voropaev.roma@gmail.com>
2018-03-04 21:00:06 +0300

    Some little fixes

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import *
 
 
-class ExerciseSerializer(serializers.HyperlinkedModelSerializer):
+class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
         fields = '__all__'
